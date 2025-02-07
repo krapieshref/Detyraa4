068cee592ec248798e156b31d2ab3a3aac6edf69
eshrefkrapi <eshref.krapi14@gmail.com>
2025-02-07 21:02:25 +0100

    Zgjedhja e opsioneve ne Menun Kryesore

--- projektifinal.cpp
+++ projektifinal.cpp
@@ -10,7 +10,26 @@ void menaxhoNotat();
 
 
 int main() {
+
     menuKryesore();
+
     return 0;
 }
+
+void menuKryesore() {
+    int zgjedhja;
+    do {
+        cout << "\nVegla per Menaxhimin e Notave te Studenteve(VMNS)\n";
+
+        cout << "3. Dil\n";
+        cout << "Zgjedhni: ";
+        cin >> zgjedhja;
+
+        switch (zgjedhja) {
+        case 3: cout << "Mirupafshim!\n"; break;
+        default: cout << "Zgjedhja nuk u gjend ne sherbimet tona. Provoni perseri.\n";
+        }
+    } while (zgjedhja != 3);
+}
+
 
