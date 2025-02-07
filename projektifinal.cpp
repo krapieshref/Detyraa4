b90209071d746a95d4f36109a96a05d85d8c25e4
AlktaThaci <alketathacii@gmail.com>
2025-02-07 21:13:48 +0100

    Funksioni per kalkulimin  e notave

--- projektifinal.cpp
+++ projektifinal.cpp
@@ -15,4 +15,18 @@ int main() {
 
     return 0;
 }
+void Kalkulatori() {
+
+    char perserit;
+
+    do
+    {
+
+        cout << "\n--- Kalkulatori ---\n";
+        int numriNotave;
+        cout << "Shkruani numrin e notave qe keni: ";
+        cin >> numriNotave;
+
+    }
+}
 
