edaa7b7cbee2522f54e7356d444fa6eaa7e00449
AlktaThaci <alketathacii@gmail.com>
2025-02-07 22:08:34 +0100

    Kontrollimi per numrin e notave

--- projektifinal.cpp
+++ projektifinal.cpp
@@ -27,6 +27,10 @@ void Kalkulatori() {
         cout << "Shkruani numrin e notave qe keni: ";
         cin >> numriNotave;
 
+    if (numriNotave <= 0) {
+        cout << "Numri i notave duhet te jete me i madh se zero.\n";
+        return;
     }
+
 }
 
